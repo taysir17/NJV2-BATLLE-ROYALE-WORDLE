f251a3d7419a8b37ade040b4c29bb9e4c741451c
Taissir <taysirbouzidi123@gmail.com>
2025-12-11 15:45:51 +0100

    Initial commit - index.html & firebase-config.js only

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Configuration Firebase - À remplacer par vos vraies clés
+const firebaseConfig = {
+  apiKey: "AIzaSyD_VOTRE_CLE_API",
+  authDomain: "wordlebattle-abc123.firebaseapp.com",
+  databaseURL: "https://wordlebattle-abc123.firebaseio.com",
+  projectId: "wordlebattle-abc123",
+  storageBucket: "wordlebattle-abc123.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:abc123def456ghi"
+};
+
+// Initialiser Firebase
+firebase.initializeApp(firebaseConfig);
